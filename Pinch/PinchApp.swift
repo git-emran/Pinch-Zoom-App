2e922b9495f156acaf0483960d3d2d8911bde11d
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-03-11 18:16:16 +0600

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Emran Hossain on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
